23534c9a68c188f7a0910ffedd3a3b51d9d1d68b
DiegoRaC14 <141955933+DiegoRaC14@users.noreply.github.com>
2023-09-07 11:26:51 -0600

    Add files via upload

--- Examen ejercicio 3.cpp
+++ Examen ejercicio 3.cpp
@@ -1,21 +1,23 @@
-/*Programa que muestra los numero impares hasta el valor ingresado*/
+/*Programa que muestra los numeros impares hasta el valor ingresado*/
+
 #include <stdio.h>
 
 int main() 
 {
     int numero; // Declarar variable
+    int i = 1; // Inicializar variable
 
     printf("Introduzca un numero entero:");
-	scanf("%d",&numero);//Ingresar numero
-	printf("Los numeros impares del 1 al %d son:\n", numero);
+    scanf("%d", &numero); // Ingresar numero
+    printf("Los numeros impares del 1 al %d son:\n", numero);
 
-    for (int i = 1; i <= numero; i++)/*Valor de i empieza en 1, incrementa de valor en 1 siempre y cuando
-    se cumpla que i<= numero que ingresamos*/
+    while (i <= numero)
 	{
-        if (i % 2 != 0) {
+        if (i % 2 != 0) 
+		{
             printf("%d\n", i);
         }
+        i++; // Aumentar i en 1 hasta 
     }
-
     return 0;
 }
